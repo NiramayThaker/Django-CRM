f5378dcf6022fe85ce480e8de7fc901e95f04324
Niramay Thaker <thakerniramay@gmail.com>
2023-09-10 16:42:43 +0530

    --

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def login(request):
+    if request.method == "POST":
+        user = request.POST(request)
+        pass
+
+    return render(request, "login.html")
+
+
+def logout(request):
+    logout(request.user)
