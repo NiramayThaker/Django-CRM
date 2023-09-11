dcb97c6f16d323494cc4f6e1c21c95fcbcf544ee
Niramay Thaker <thakerniramay@gmail.com>
2023-09-11 11:33:05 +0530

    --

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,19 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
-
-
-def login(request):
     if request.method == "POST":
-        user = request.POST(request)
-        pass
+        username = request.POST['username']
+        password = request.POST['pswd']
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successfull")
+            return redirect('home')
+        else:
+            messages.error(request, "Couldn't log in")
+            return redirect('home')
+    else:
+        return render(request, "home.html")
 
-    return render(request, "login.html")
+
+# def login(request):
+#     pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "User logged out.!")
+    return redirect('home')
 
 
-def logout(request):
-    logout(request.user)
+def register_user(request):
+    return render(request, "user_reg.html")
