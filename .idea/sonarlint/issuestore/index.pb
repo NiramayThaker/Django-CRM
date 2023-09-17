df6919b8744773df6cadda17c20a03b5224504a1
NiramayThaker <thakerniramay@gmail.com>
2023-09-17 14:17:01 +0530

    --

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,13 @@ J
 dcrm/templates/navbar.html,e\7\e7bfbf63561c6ba5e70de06c3c434851f5640dc6
 I
 dcrm/templates/login.html,a\3\a38782efc05a0fd2b19b01075e3e4a77cf544548
+D
+dcrm/website/apps.py,9\5\95868cf8b44c9fb3f8d1f31a1e54422821019a8c
+E
+dcrm/dcrm/settings.py,d\a\da830aed5d86c9c3a62e58b8c7726e243073be1b
+L
+dcrm/templates/user_reg.html,3\4\341280f8fd2d761ad20efd3aa3c25a40fb83c0df
+E
+dcrm/website/admin.py,1\6\16dff5e9694e5c7837c93e2832ba82f279694125
+?
+dcrm/db.sqlite3,0\9\092ad9c8b17e94d0258343d378604fa2b42676ab
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,51 +1,60 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from . import forms
+from .models import Record
+
 
 @login_required(login_url='login/')
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+	records = Record.objects.all()
+
+	context = {'records': records}
+	return render(request, "home.html", context=context)
 
 
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['pswd']
-        user = authenticate(username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            messages.success(request, "Login Successfull")
-            return redirect('home')
-        else:
-            messages.error(request, "Couldn't log in")
-            return redirect('home')
-        
-    return render(request, 'login.html')
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['pswd']
+		user = authenticate(username=username, password=password)
+		print(user)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Login Successfull")
+			return redirect('home')
+		else:
+			messages.error(request, "Couldn't log in")
+			return redirect('home')
+
+	return render(request, 'login.html')
 
 
 def logout_user(request):
-    logout(request)
-    messages.success(request, "User logged out.!")
-    return redirect('home')
+	logout(request)
+	messages.success(request, "User logged out.!")
+	return redirect('home')
 
 
 def register_user(request):
-    reg_form = forms.RegistrationForm()
-    if request.method == "POST":
-        reg_form = forms.RegistrationForm(request.POST)
-        if reg_form.is_valid():
-            user = reg_form.save()
-            if user is not None:
-                login(request, user)
-                messages.success(request, "User registered successfully")
-                return redirect('home')
-            else:
-                messages.success(request, "Can't registered user")
-                return redirect('register')         
-            
-    context = {'reg_form': reg_form}
-    return render(request, "user_reg.html", context=context)
+	reg_form = forms.RegistrationForm()
+	if request.method == "POST":
+		reg_form = forms.RegistrationForm(request.POST)
+		if reg_form.is_valid():
+			user = reg_form.save()
+			if user is not None:
+				login(request, user)
+				messages.success(request, "User registered successfully")
+				return redirect('home')
+			else:
+				messages.success(request, "Can't registered user")
+				return redirect('register')
+
+	context = {'reg_form': reg_form}
+	return render(request, "user_reg.html", context=context)
+
+
+def customer_record(request, pk):
+	pass
